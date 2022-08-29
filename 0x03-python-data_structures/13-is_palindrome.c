c43713faa35db12acd3490d60f2293410ad41024
MissTipo <dorine.a.tipo@gmail.com>
2022-08-28 21:13:48 -0700

    created a function to check if a linked list is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,24 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
 /**
- * is_palindrome - checks if a linked list is a palindrome
- *
- * @head: head of the node
- * Return: 0 if false, 1 if true
+ * is_palindrome - function to call check_pal to see if list is palindrome
+ * @head: ptr to the beginning of the list
+ * Return: 0 if not palindrome else 1
  */
-
 int is_palindrome(listint_t **head)
 {
-	unsigned int list_len;
-	listint_t *tmp;
-
 	if (head == NULL || *head == NULL)
 		return (1);
-	tmp = *head;
-	while (tmp)
+	return (check_pal(head, *head));
+}
+
+/**
+ * check_pal - function to check if the list is palindrome
+ * @head: ptr to the beginning of the list
+ * @last: ptr to the end of the list
+ * Return: 0 if not palindrom else 1
+ */
+int check_pal(listint_t **head, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+	if (check_pal(head, last->next) && (*head)->n == last->n)
 	{
-		tmp = tmp->next;
-		list_len++;
+		*head = (*head)->next;
+		return (1);
 	}
 	return (0);
 }
