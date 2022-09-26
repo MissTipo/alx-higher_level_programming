cd90ba8e727e820ae324c73874728c52d533eccb
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 04:59:19 -0700

    fixing wrong out put

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ A class that inherits from the class list
 
 class MyList(list):
     """Derived class"""
-    def __init__(self):
-        """initializes the attributes of MyList class"""
-        super().__init__()
-
 
     def print_sorted(self):
         """Prints sorted list in ascending order"""
