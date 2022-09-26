5448fb322d5be225b801fe77ff8efbe59eb59420
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 04:40:22 -0700

    created a class that inherits from another class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A class that inherits from the class list
+"""
+
+
+class MyList(list):
+    """Derived class"""
+    def __init__(self):
+        """initializes the attributes of MyList class"""
+        super().__init__(self)
+
+
+    def print_sorted(self):
+        """Prints sorted list in ascending order"""
+        print(sorted(self))
