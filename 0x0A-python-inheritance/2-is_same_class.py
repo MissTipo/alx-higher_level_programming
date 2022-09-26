87ee01280868e0ebf7e0d57c731303ebe7dbb7d7
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 08:05:43 -0700

    Added a blank line

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,7 @@ Returns True if the object is exactly an instace
 of the specified class, otherwise False
 """
 
+
 def is_same_class(obj, a_class):
     """Returns true if an object in instance of a class"""
     return type(obj) == a_class
