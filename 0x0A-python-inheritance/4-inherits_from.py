88f1b599bbb2e39ea9ec70646da6ad065396b6eb
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 08:57:07 -0700

    Added a requirement

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,13 @@ from the specified class ; otherwise False
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an instance of subclass"""
+    """Checks if an object is an instance of subclass
+    Arguments:
+    obj: The object to check
+    a_class: The class to match the obj to
+    Returns:
+    True if is instance, otherwise false
+    """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
