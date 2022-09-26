866fa93a89691d4a96f72973a83c9a9917625a1d
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 08:39:53 -0700

    Added a second parameter to issubclass function

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Returns True if the object is an instance of
+a class that inherited directly or indirectly
+from the specified class ; otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of subclass"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
