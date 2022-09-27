33d609fdc433919d406f97dc9fc31498d1102018
MissTipo <dorine.a.tipo@gmail.com>
2022-09-27 08:54:34 -0700

    reading a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,9 @@
 Contains a function that reads a text file
 """
 
-def read_file(filename=""):def read_file(filename=""):
+
+def read_file(filename=""):
     """reads a text file and prints it out"""
-    with open(filename, "r", encoding=("utf-8")) as f:
+    with open("UTF8", "r", encoding=("utf-8")) as f:
         read_data = f.read()
     print(read_data, end='')
