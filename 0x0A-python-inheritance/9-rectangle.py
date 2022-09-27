e80f205297b239d2549af0dc3c8f05f8c5bb1f6f
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 23:10:27 -0700

    Added dunderscore to the str function

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle(BaseGeometry):
     def area(self):
         """returns the area of the class Rectangle"""
         return self.__width * self.__height
+
     def __str__(self):
         """returns description of the class Rectangle"""
-        return "[Rectangle]{}/{}".format(self.__width, self.__height)
+        return "[Rectangle]{:d}/{:d}".format(self.__width, self.__height)
