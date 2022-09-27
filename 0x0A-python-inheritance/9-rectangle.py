044423039654f9d87f1822fdadb6d42d55e8c479
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 23:08:20 -0700

    Added dunderscore to the str function

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,18 +4,7 @@ Contains a derived class
 """
 
 
-class BaseGeometry:
-    """a class with public method that raises exception"""
-    def area(self):
-        """a public instance method that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """a public instance method that validates value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """a derived class"""
