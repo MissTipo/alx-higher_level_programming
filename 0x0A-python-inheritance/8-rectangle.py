53d1893e46708cd94fc882169cfcdafe41980358
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 22:22:05 -0700

    Validating the parameters width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,6 +11,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """a function that instantiates class attributes"""
         self.integer_validator("width", width)
-        self.width = width
+        self.__width = width
         self.integer_validator("height", height)
-        self.height = height
+        self.__height = height
