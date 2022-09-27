e8079e4cc42363d15f5b45b2185d1ed486b093da
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 23:57:18 -0700

    fixed an atrribute error

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Contains a derived class Square that inherits
+from the base class Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from the class Rectangle"""
+    def __init__(self, size):
+        """Instantiates the attributes of class Rectangle"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """returns the area of class square"""
+        return self.__size ** 2
