58b27e89849628de9b2bbfd2a96f199002b19563
MissTipo <dorine.a.tipo@gmail.com>
2022-09-26 22:12:29 -0700

    created a class that inherits from the base class BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains a derived class
+"""
+
+class BaseGeometry:
+    """this is a base class"""
+
+class Rectangle(BaseGeometry):
+    """this class inherits from the class baseGeometry"""
+    def __init__(self, width, height):
+        """a function that instantiates class attributes"""
