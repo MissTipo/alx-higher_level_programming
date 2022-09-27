d137556bb858d77aa489be91ce8be0387c547cc7
MissTipo <dorine.a.tipo@gmail.com>
2022-09-27 10:09:51 -0700

    removing trailing white space

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-Contains a function that returns the JSON representation of an object 
+Contains a function that returns the JSON representation of an object
 """
 
 
 import json
-
 
 
 def to_json_string(my_obj):
