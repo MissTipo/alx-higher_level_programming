48bf89fcd57eea33467a9b9e3e9d0d67ac1c7fcd
MissTipo <dorine.a.tipo@gmail.com>
2022-09-28 09:03:50 -0700

    fix pycodestyle error

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-Contains a derived class Square that inherits
-from the class Rectangle
+Contains the class BaseGeometry and subclass Rectangle
 """
-
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """inherits from the class Rectangle"""
+    """A representation of a square"""
     def __init__(self, size):
-        """Instantiates the attributes of class Square"""
+        """instantiation of the square"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
