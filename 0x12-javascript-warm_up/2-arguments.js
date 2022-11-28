7cf776d20f80a526c85f931442fa8bb661f5afd9
MissTipo <dorine.a.tipo@gmail.com>
2022-11-28 18:22:41 +0300

    print a message depending on the number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const argc = process.argv.length;
 if (argc < 3) {
   console.log('No argument');
-}else if (argc === 3) {
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
