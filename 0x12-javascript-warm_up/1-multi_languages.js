71c1b0e276a006582ae9a7dd51a95f68dcee6558
MissTipo <dorine.a.tipo@gmail.com>
2022-11-28 17:04:37 +0300

    print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints 3 lines
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
+/* let x = "C is fun";
+let y = "Python is cool";
+let z = "JavaScript is amazing";
+console.log($(x)\n$(y)\n$(z)); */
