efaf901a54095ddc87df5d150a784a04267b46eb
MissTipo <dorine.a.tipo@gmail.com>
2022-11-29 18:35:58 +0300

    fix errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 // A class Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) /* | typeof (h) === 'number' && typeof (w) === 'number')*/ {
+    if (w > 0 && h > 0) /* | typeof (h) === 'number' && typeof (w) === 'number') */ {
       this.width = w;
       this.height = h;
     }
