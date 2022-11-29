7c24ebb6e3a1d027b3f2d3733aa4dd72e8c5689c
MissTipo <dorine.a.tipo@gmail.com>
2022-11-29 18:06:30 +0300

    class rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// A class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 | typeof (h) === 'number' && typeof (w) === 'number') {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
