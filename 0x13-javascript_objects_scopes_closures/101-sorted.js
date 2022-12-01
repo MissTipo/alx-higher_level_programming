c08966b7618a05668a5db3708c89110df6ab510c
MissTipo <dorine.a.tipo@gmail.com>
2022-12-01 05:34:08 +0300

    fix errors

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
-const { dict } = require('./101-data');
-
-const newd = {};
-let list = [];
-for (const [key, value] of Object.entries(dict)) {
-  if (!(value in newd)) {
-    list = [];
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
   } else {
-    list = newd[value];
+    newDict[dict[key]].push(key);
   }
-  list.push(key);
-  newd[value] = list;
 }
-console.log(newd);
+console.log(newDict);
+/* exports.dict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+}; */
