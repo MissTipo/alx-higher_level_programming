515146577e87831a7279d74882cb8b1694e453f0
MissTipo <dorine.a.tipo@gmail.com>
2022-12-01 05:04:42 +0300

    fix errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-
 const fs = require('fs');
-
-fs.readFile(myArgs[0], 'utf-8', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  fs.readFile(myArgs[1], 'utf-8', function (err2, data2) {
-    if (err2) {
-      throw err2;
-    }
-    const str = data + data2;
-    fs.writeFile(myArgs[2], str, function () {});
-  });
-});
+const x = fs.readFileSync(process.argv[2], 'utf-8');
+const y = fs.readFileSync(process.argv[3], 'utf-8');
+fs.writeFileSync(process.argv[4], x + y);
