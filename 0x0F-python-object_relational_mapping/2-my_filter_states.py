d6ab5acc80b782fcb7bfc09be1f9e79fed1318f0
MissTipo <dorine.a.tipo@gmail.com>
2022-12-04 07:09:46 +0300

    Fix errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,21 @@
-#!/usr/bin/ python3
+#!/usr/bin/python3
 """
-script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+python script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
 """
-import sys
-from sys import argv
+
 import MySQLdb
-# db = MySQLdb.connect(mysql username=sys.argv[0],mysql
-# password=sys.argv[1],database name=sys.argv[3],localhost=3306)
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3],
-        port=3306)
-    # staname = argv[4]
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{:s}' ORDER BY id ASC"
-        .format(sys.argv[4]))
-    # cur.execute(f"SELECT * FROM states WHERE id = {sys.argv[4]}")
-
-    states = cur.fetchall()
-
-    for state in states:
-        """if state[1] == sys.argv[4]:
-            print(state)"""
-        print(state)
-
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
