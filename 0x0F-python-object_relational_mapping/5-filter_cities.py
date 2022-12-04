e73438f97ac5836f57194bee3cdbb37f3b826927
MissTipo <dorine.a.tipo@gmail.com>
2022-12-04 05:57:55 +0300

    Fix errors

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,20 @@
-#!/usr/bin/ python3
+#!/usr/bin/python3
 """
-script that takes in the name of a state as an argument and lists
-all cities of that state, using the database hbtn_0e_4_usa
+python script that akes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
 """
-import sys
-from sys import argv
+
 import MySQLdb
-# db = MySQLdb.connect(mysql username=sys.argv[0],mysql
-# password=sys.argv[1],database name=sys.argv[3],localhost=3306)
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        passwd=argv[2],
-        database=argv[3],
-        port=3306)
-    arg = argv[4]
-    cur = db.cursor()
-    """cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM cities INNER JOIN states ON cities.id=states.id\
-            ORDER BY cities.id")"""
+from sys import argv
 
-    cur.execute("SELECT cities.name \
-                FROM cities \
-                JOIN states ON cities.state_id = states.id \
-                WHERE states.name LIKE BINARY '{:s}' ORDER BY cities.id ASC".format(argv[4],)
-                )
-    # cur.execute(f"SELECT * FROM states WHERE id = {sys.argv[4]}")
-
-    states = cur.fetchall()
-    print(",".join(city[0] for city in states))
-
-    """for city in states:
-        if states is not None:
-            print(",".join[city])"""
-
-    cur.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
+    rows = cursor.fetchall()
+    print(", ".join(city[0] for city in rows))
+    cursor.close()
     db.close()
