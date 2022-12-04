a3365d90b11119f8dda104d402fe102e9e6728c8
MissTipo <dorine.a.tipo@gmail.com>
2022-12-04 06:02:05 +0300

    Fix errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/ python3
 """script that lists all cities from the database hbtn_0e_4_usa"""
+
+
 import sys
 from sys import argv
 import MySQLdb
-# db = MySQLdb.connect(mysql username=sys.argv[0],mysql
-# password=sys.argv[1],database name=sys.argv[3],localhost=3306)
+
 if __name__ == '__main__':
     db = MySQLdb.connect(
         user=sys.argv[1],
@@ -13,10 +14,6 @@ if __name__ == '__main__':
         port=3306)
     #arg = argv[4]
     cur = db.cursor()
-    """cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM cities INNER JOIN states ON cities.id=states.id\
-            ORDER BY cities.id")"""
-
     cur.execute("SELECT cities.id, cities.name, states.name \
                 FROM cities \
                 JOIN states ON cities.state_id = states.id"
