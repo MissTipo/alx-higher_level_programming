e0d711324d46dad8ceb708b14b03b6e38350184b
MissTipo <dorine.a.tipo@gmail.com>
2022-12-15 13:10:26 +0300

    1 body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes url and displays the body of response
-body=$( curl -sI "$1" | grep -i 'HTTP/1.1' |cut -d " " -f2 ); if [ "$body" -eq "200" ]; then curl "$1"; fi
+curl -sI "$1"
