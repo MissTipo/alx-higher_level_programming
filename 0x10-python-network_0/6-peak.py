2a03e7d0835479f7c6f7f733f49112c3f15c8290
MissTipo <dorine.a.tipo@gmail.com>
2022-12-16 10:10:02 +0300

    fix errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,10 @@
 """Finds a peak in a list of unsorted integers"""
 # Divide and conquer
 # find length of list and divide by 2
-# find peak of the first part of the list and compare with the peak of the second list
+# find peak of the first part of the list and compare
+# with the peak of the second list
 # id peak1 > peak2 return peak1
+
 
 def find_peak(list_of_integers):
     if list_of_integers == []:
@@ -15,10 +17,11 @@ def find_peak(list_of_integers):
         return max(list_of_integers)
     mid_val = int(list_len / 2)
     peak = list_of_integers[mid_val]
-    if peak > list_of_integers[mid_val - 1] and peak > list_of_integers[mid_val + 1]:
+    if peak > list_of_integers[mid_val -
+                               1] and peak > list_of_integers[mid_val + 1]:
+        # print(peak)
         return peak
     elif peak < list_of_integers[mid_val - 1]:
         return find_peak(list_of_integers[:mid_val])
     else:
         return find_peak(list_of_integers[mid_val + 1:])
-
